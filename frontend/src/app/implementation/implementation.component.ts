23d516f46c55352d4d90cac1ef1a8e74b89ccbe4
CoreyCole <coreyleoc@gmail.com>
2017-08-21 00:19:47 -0700

    chore(frontend): adds more documentation

--- frontend/src/app/implementation/implementation.component.ts
+++ frontend/src/app/implementation/implementation.component.ts
@@ -16,13 +16,13 @@ export class ImplementationComponent implements OnInit {
       `,
       code: `
         FUNCTION matchit(
-          sourceTable TEXT,        -- input table name
-          primaryKey TEXT,         -- source table's primary key
-          treatmentsArr TEXT[],    -- array of treatment column names
-          covariatesArr TEXT[],    -- array of covariate column names
-                                   -- (all covariates are applied to all treatments)
-          outputTable TEXT         -- output table name
-        ) RETURNS TEXT
+          sourceTable TEXT,     -- input table name
+          primaryKey TEXT,      -- source table's primary key
+          treatmentsArr TEXT[], -- array of treatment column names
+          covariatesArr TEXT[], -- array of covariate column names
+                                -- (all covariates are applied to all treatments)
+          outputTable TEXT      -- output table name
+        ) RETURNS TEXT          -- RETURNS function call status string
       `,
     },
     {
@@ -40,7 +40,7 @@ export class ImplementationComponent implements OnInit {
           treatmentLevels INTEGER, -- possible levels for given treatment
           covariatesArr TEXT[],    -- array of covariate column names for given treatment
           outputTable TEXT         -- output table name
-        ) RETURNS TEXT
+        ) RETURNS TEXT             -- RETURNS function call status string
       `
     },
     {
@@ -58,7 +58,30 @@ export class ImplementationComponent implements OnInit {
           treatmentsArr TEXT[],         -- array of treatment column names
           covariatesArraysArr TEXT[][], -- array of arrays of covariates, each treatment has its own set of covariates
           outputTableBasename TEXT      -- name used in all output tables, treatment appended
-        ) RETURNS TEXT
+        ) RETURNS TEXT                  -- RETURNS function call status string
+      `
+    },
+    {
+      name: 'Two Table Matchit',
+      link: 'two_table_matchit',
+      description: `
+        This form of matchit supports joining data from two tables and matching on 1 binary treatment.
+        Prunes rows from input table A first to optimize join.
+        The results are materialized in a view.
+      `,
+      code: `
+        CREATE OR REPLACE FUNCTION two_table_matchit(
+          sourceTableA TEXT,           -- input table A name
+          sourceTableAPrimaryKey TEXT, -- input table A primary key
+          sourceTableAForeignKey TEXT, -- foreign key linking to input table B
+          covariatesArrA TEXT[],       -- covariates included in input table A
+          sourceTableB TEXT,           -- input table B name
+          sourceTableBPrimaryKey TEXT, -- input table B primary key
+          covariatesArrB TEXT[],       -- covariates included in input table B
+          treatment TEXT,              -- treatment column must be in sourceTableA
+          treatmentLevels INTEGER,     -- possible levels for given treatment
+          outputTable TEXT             -- output table name
+        ) RETURNS TEXT                 -- RETURNS function call status string
       `
     },
   ];
