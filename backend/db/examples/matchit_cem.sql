c5d0070ba738401abd10cbbdd28d014306d3b4e8
Corey Cole <coreyleoc@gmail.com>
2018-03-10 18:18:01 -0800

    feat(db): add matchit_cem example mat view

--- backend/db/examples/matchit_cem.sql
+++ backend/db/examples/matchit_cem.sql
@@ -1,6 +1,6 @@
 DROP MATERIALIZED VIEW IF EXISTS test_flight;
 
-SELECT matchit_cem('demo_data_1000000', 'fid', ARRAY['lowpressure'], ARRAY['rain', 'fog'], 'test_flight');
+SELECT matchit_cem('demo_data_1000000', 'fid', ARRAY['carrier', 'lowpressure'], ARRAY['rain', 'fog'], 'test_flight');
 
 SELECT lowpressure, rain_matched, fog_matched, count(*), avg(depdelay)
 	FROM test_flight GROUP BY lowpressure, rain_matched, fog_matched 
@@ -9,3 +9,27 @@ SELECT lowpressure, rain_matched, fog_matched, count(*), avg(depdelay)
 SELECT * FROM test_flight;
 
 DROP FUNCTION matchit_cem(text,text,text[],text[],text);
+
+SELECT matchit_cem(
+  'demo_data_1000000',
+  'fid',
+  ARRAY['carrier', 'lowpressure'],
+  ARRAY['rain', 'fog'],
+  'test_flight'
+); 
+
+CREATE MATERIALIZED VIEW test_flight AS
+WITH subclasses AS 
+    (SELECT max(fid) AS subclass_fid,
+         rain AS rain_matched,
+         fog AS fog_matched
+    FROM demo_data_1000000
+    GROUP BY  rain_matched, fog_matched
+    HAVING (count(DISTINCT carrier) = 20
+            OR count(DISTINCT lowpressure) = 2))
+SELECT *
+FROM subclasses, demo_data_1000000 st
+WHERE subclasses.rain_matched = st.rain
+        AND subclasses.fog_matched = st.fog
+        AND lowpressure IS NOT NULL
+WITH DATA;
