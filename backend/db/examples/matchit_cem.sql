f3be8416e901238af3e04a8f0ccaf83216ef7624
Corey Cole <coreyleoc@gmail.com>
2018-03-16 18:30:58 -0700

    feat: remove multiple treatments (arr param) from cem, only support 1 treatment in matchit_cem

--- backend/db/examples/matchit_cem.sql
+++ backend/db/examples/matchit_cem.sql
@@ -13,23 +13,21 @@ DROP FUNCTION matchit_cem(text,text,text[],text[],text);
 SELECT matchit_cem(
   'demo_data_1000000',
   'fid',
-  ARRAY['carrier', 'lowpressure'],
+  'lowpressure',
   ARRAY['rain', 'fog'],
   'test_flight'
 ); 
 
-CREATE MATERIALIZED VIEW test_flight AS
+CREATE TABLE test_flight AS
 WITH subclasses AS 
-    (SELECT max(fid) AS subclass_fid,
-         rain AS rain_matched,
-         fog AS fog_matched
-    FROM demo_data_1000000
-    GROUP BY  rain_matched, fog_matched
-    HAVING (count(DISTINCT carrier) = 20
-            OR count(DISTINCT lowpressure) = 2))
+  (SELECT max(fid) AS subclass_id,
+    rain AS rain_matched,
+    fog AS fog_matched
+  FROM demo_data_1000000
+  GROUP BY  rain_matched, fog_matched
+  HAVING count(DISTINCT lowpressure) = 2)
 SELECT *
 FROM subclasses, demo_data_1000000 st
 WHERE subclasses.rain_matched = st.rain
-        AND subclasses.fog_matched = st.fog
-        AND lowpressure IS NOT NULL
-WITH DATA;
+  AND subclasses.fog_matched = st.fog
+  AND lowpressure IS NOT NULL
