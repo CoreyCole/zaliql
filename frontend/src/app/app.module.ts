80adf6a07b46cee2394331b47804d7972d2988ec
CoreyCole <coreyleoc@gmail.com>
2018-09-13 21:11:24 -0700

    feat(frontend): authors and contributors

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -19,7 +19,6 @@ import {
   MatCardModule,
   MatListModule,
   MatSlideToggleModule,
-  MatSidenav,
   MatSidenavModule,
   MatInputModule,
   MatSelectModule,
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public authors = [
+    {
+      name: 'Corey Cole',
+      email: 'coreylc@uw.edu'
+    },
+    {
+      name: 'Babak Salimi',
+      email: 'bsalimi@cs.washington.edu'
+    },
+    {
+      name: 'Dan Suciu',
+      email: 'suciu@cs.washington.edu'
+    },
+    {
+      name: 'Dan R. K. Ports',
+      email: 'drkp@cs.washington.edu'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  mailTo(email: string) {
+    window.location.href = `mailto:${email}`;
   }
 
 }
